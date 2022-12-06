4a54e8ddcd4872b2f96da3efe67ee5f1a84ecc74
stganesh14 <stganesh1999@gmail.com>
2022-12-06 22:44:16 +0530

    Commit Message

---
+++ src/components/headers/Header.js
@@ -0,0 +1,101 @@
+import React, {useContext, useState} from 'react'
+import {GlobalState} from '../../GlobalState'
+import Menu from './icon/menu.svg'
+import Close from './icon/close.svg'
+import Cart from './icon/cart.svg'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+
+function Header() {
+    const state = useContext(GlobalState)
+    const [isLogged] = state.userAPI.isLogged
+    const [isAdmin] = state.userAPI.isAdmin
+    const [cart] = state.userAPI.cart
+    const [menu, setMenu] = useState(false)
+
+    const logoutUser = async () =>{
+        await axios.get('/user/logout')
+        
+        localStorage.removeItem('firstLogin')
+        
+        window.location.href = "/";
+    }
+
+    const sellProduct = () => {
+      if (!isLogged) return alert("Login to post your product");
+      let link =
+        "mailto:stganesh1999@gmail.com" +
+        "?cc=stganesh1999@gmail.com" +
+        "&subject=" +
+        escape("Sell my product") +
+        "&body=" +
+        escape(`Give us a below details: title,seller name,price,description,address,contact,Product image`);
+      window.location.href = link;
+    };
+
+    const adminRouter = () =>{
+        return(
+            <>
+                <li><Link to="/create_product">Create Product</Link></li>
+                <li><Link to="/category">Categories</Link></li>
+            </>
+        )
+    }
+
+    const loggedRouter = () =>{
+        return(
+            <>
+                <li><Link to="/history">History</Link></li>
+                <li><Link to="/" onClick={logoutUser}>Logout</Link></li>
+            </>
+        )
+    }
+
+
+    const styleMenu = {
+        left: menu ? 0 : "-100%"
+    }
+
+    return (
+        <header>
+            <div className="menu" onClick={() => setMenu(!menu)}>
+                <img src={Menu} alt="" width="30" />
+            </div>
+
+            <div className="logo">
+                <h1>
+                    <Link to="/">{isAdmin ? 'Admin' : 'Foodie Choice'}</Link>
+                </h1>
+            </div>
+
+            <ul style={styleMenu}>
+                <li><Link to="/">{isAdmin ? 'Products' : 'Shop'}</Link></li>
+                <li onClick={sellProduct}>{isAdmin ? '' : 'Sell Your Product'}</li>
+
+                {isAdmin && adminRouter()}
+
+                {
+                    isLogged ? loggedRouter() : <li><Link to="/login">Login ✥ Register</Link></li>
+                }
+
+                <li onClick={() => setMenu(!menu)}>
+                    <img src={Close} alt="" width="30" className="menu" />
+                </li>
+
+            </ul>
+
+            {
+                isAdmin ? '' 
+                :<div className="cart-icon">
+                    <span>{cart.length}</span>
+                    <Link to="/cart">
+                        <img src={Cart} alt="" width="30" />
+                    </Link>
+                </div>
+            }
+            
+        </header>
+    )
+}
+
+export default Header
